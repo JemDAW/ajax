5fc0b1035f4f4f6409d7d8e1774053e5dd6cab2f
JemDAW <jemimamolina.alu@iespacomolla.es>
2020-11-23 18:19:16 +0100

    Gif funcionalidad acabada

--- ajax.js
+++ ajax.js
@@ -1,8 +1,9 @@
 const key = 'yv7AE1nYWMcjPtGryWIkjGZXfmJzzNRv';
-const body = document.querySelector('body');
+const div = document.getElementById('contenedorGif');
 
+div.style.opacity = 0;
 var botonBuscar= document.getElementById("buscarBoton");
-botonBuscar.addEventListener('click', busqueda,false);
+botonBuscar.addEventListener('click', busqueda, false);
 
 const getJSON = function (url){
     return fetch(url).then(response => {
@@ -10,15 +11,23 @@ const getJSON = function (url){
     });
 }
 
+
 function busqueda(){
     let elementoBuscar = document.getElementById("buscar").value;
     getJSON(`http://api.giphy.com/v1/gifs/search?q=${elementoBuscar}&api_key=${key}&limit=5`)
     .then(data =>{
-        renderizarGif(data[0]);
+        let arrayDatos= data.data;
+        div.innerHTML = " ";
+        for(let i=0; i<arrayDatos.length; i++){
+        renderizarGif(arrayDatos, i);
+        }
     })
 }
 
-function renderizarGif(data){
-    const html = `<img src="${data.url}" />`
-    body.insertAdjacentHTML('beforeend', html);
+
+function renderizarGif(data, numImg){
+    let imagen = data[numImg].images.downsized;
+    let html = `<img src="${imagen.url}" />`;
+    div.insertAdjacentHTML('beforeend', html);
+    div.style.opacity = 1;
 }
